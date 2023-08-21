eb56d05aad9e97aa7bd6edfb99f33973a7f317c3
MarioGoDevLike <marionassar57@gmail.com>
2023-08-21 11:54:05 +0300

    added Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='py-8'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          <a className='text-purple-500 font-bold font-serif text-2xl w-10 tracking-wide leading-6'  href="#">
+            MARIO NASSAR
+          </a>
+          <button className='btn btn-sm'>Work with me</button>
+        </div>
+      </div>
+
+    </header>
+  )
 };
 
 export default Header;
